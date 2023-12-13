fc7552e6717442dc7300de38420caba9cef75a31
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2023-12-13 18:37:46 +0530

    Till complete

--- lib/authentication/login_page.dart
+++ lib/authentication/login_page.dart
@@ -12,159 +12,202 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  //
   Color emailBorderColor = MyColor.orangeO;
   Color passwordBorderColor = MyColor.orangeO;
   Color emailHintColor = MyColor.orangeO;
   Color passwordHintColor = MyColor.orangeO;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset('assets/images/logo.png'),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 20.0),
-              child: Text(
-                'ID',
-                style: MyStyle.tx13O,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('assets/images/logo.png'),
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: emailBorderColor,
+              const SizedBox(
+                height: 60,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text(
+                  'ID',
+                  style: MyStyle.tx13O,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: emailBorderColor,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(),
+                    hintText: 'Please enter your email.',
+                    hintStyle: TextStyle(
+                      color: emailHintColor,
                     ),
                   ),
-                  border: const OutlineInputBorder(),
-                  hintText: 'Please enter your email.',
-                  hintStyle: TextStyle(
-                    color: emailHintColor,
-                  ),
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    setState(() {
+                      emailBorderColor = (value.isEmpty
+                          ? MyStyle.tx14OP
+                          : MyStyle.tx14O) as Color;
+                      emailHintColor = (value.isEmpty
+                          ? MyStyle.tx14OP
+                          : MyStyle.tx14O) as Color;
+                    });
+                  },
                 ),
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  setState(() {
-                    emailBorderColor = (value.isEmpty
-                        ? MyStyle.tx14OP
-                        : MyStyle.tx14O) as Color;
-                    emailHintColor = (value.isEmpty
-                        ? MyStyle.tx14OP
-                        : MyStyle.tx14O) as Color;
-                  });
-                },
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 20.0),
-              child: Text(
-                'Password',
-                style: MyStyle.tx13O,
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text(
+                  'Password',
+                  style: MyStyle.tx13O,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: passwordBorderColor,
+              const SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: passwordBorderColor,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(),
+                    hintText: 'Please enter a password.',
+                    hintStyle: TextStyle(
+                      color: passwordHintColor,
                     ),
                   ),
-                  border: const OutlineInputBorder(),
-                  hintText: 'Please enter a password.',
-                  hintStyle: TextStyle(
-                    color: passwordHintColor,
-                  ),
+                  controller: passwordController,
+                  onChanged: (value) {
+                    setState(() {
+                      passwordBorderColor = (value.isEmpty
+                          ? MyStyle.tx14OP
+                          : MyStyle.tx14O) as Color;
+                      passwordHintColor = (value.isEmpty
+                          ? MyStyle.tx14OP
+                          : MyStyle.tx14O) as Color;
+                    });
+                  },
                 ),
-                controller: passwordController,
-                onChanged: (value) {
-                  setState(() {
-                    passwordBorderColor = (value.isEmpty
-                        ? MyStyle.tx14OP
-                        : MyStyle.tx14O) as Color;
-                    passwordHintColor = (value.isEmpty
-                        ? MyStyle.tx14OP
-                        : MyStyle.tx14O) as Color;
-                  });
-                },
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20.0,
-                right: 20.0,
-                top: 20,
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (emailController.text.isNotEmpty &&
-                      passwordController.text.isNotEmpty) {}
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: (emailController.text.isNotEmpty &&
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20.0,
+                  right: 20.0,
+                  top: 20,
+                ),
+                // child: ElevatedButton(
+                //   onPressed: () {
+                //     if (emailController.text.isNotEmpty &&
+                //         passwordController.text.isNotEmpty) {}
+                //   },
+                //   style: ElevatedButton.styleFrom(
+                //       backgroundColor: (emailController.text.isNotEmpty &&
+                //               passwordController.text.isNotEmpty)
+                //           ? MyColor.orangeGrad
+                //           : MyColor.orangeO,
+                //       shape: const RoundedRectangleBorder(
+                //         borderRadius: BorderRadius.zero,
+                //       )),
+                //   child: const Text(
+                //     'Log In',
+                //     style: MyStyle.tx14W,
+                //   ),
+                // ),
+                child: InkWell(
+                  onTap: () {
+                    if (_formKey.currentState?.validate() == true) {
+                      // Perform login logic here
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Process Data'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 320,
+                    decoration: (emailController.text.isNotEmpty ||
                             passwordController.text.isNotEmpty)
-                        ? MyColor.orangeO
-                        : MyColor.orange,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
-                    )),
-                child: const Text(
-                  'Log In',
-                  style: MyStyle.tx14W,
+                        ? const BoxDecoration(gradient: MyColor.orangeGrad)
+                        : BoxDecoration(gradient: MyColor.inOrangeGrad),
+                    child: const Center(
+                      child: Text(
+                        'Log In',
+                        style: MyStyle.tx14W,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 5.0,
-            ),
-            const Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Forgot ID/Password?',
-                style: MyStyle.tx10,
+              const SizedBox(
+                height: 5.0,
               ),
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset('assets/images/kakao.png'),
-                Image.asset('assets/images/naver.png'),
-                Image.asset('assets/images/google.png'),
-                Image.asset('assets/images/apple.png'),
-              ],
-            ),
-            const SizedBox(
-              height: 5.0,
-            ),
-            const Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Do you need general membership registration?',
-                style: MyStyle.tx10,
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Forgot ID/Password?',
+                  style: MyStyle.tx10,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/kakao.png'),
+                  Image.asset('assets/images/naver.png'),
+                  Image.asset('assets/images/google.png'),
+                  Image.asset('assets/images/apple.png'),
+                ],
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Do you need general membership registration?',
+                  style: MyStyle.tx10,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/values/MyColor.dart
+++ lib/values/MyColor.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 
 class MyColor {
   static const Color orange = Color(0xFFE66100);
-  static Color orangeO = const Color(0xFFE66100).withOpacity(0.7);
-  static const Color yellow = Color(0xFFE63100);
+  static Color orangeO = const Color(0xFFE66100).withOpacity(0.3);
+  static const Color dred = Color(0xFFE63100);
   static const Color lorange = Color(0xFFEE8D03);
   static const Color dorange = Color(0xFFF2B705);
   static const Color lred = Color(0xFFE64646);
   static const Color iosblue = Color(0xFF007AFF);
   static const Color black = Color(0xFF303030);
   static Color blackO = const Color(0xFF303030).withOpacity(0.3);
-  static const LinearGradient orangeGrad = LinearGradient(colors: [
-    Color(0xFFF2B705),
-    Color(0xFFEE8D03),
-    Color(0xFFE63100),
+  static const LinearGradient orangeGrad = LinearGradient(
+    colors: [
+      Color(0xFFF2B705),
+      Color(0xFFEE8D03),
+      Color(0xFFE63100),
+    ],
+  );
+  static LinearGradient inOrangeGrad = LinearGradient(colors: [
+    Color(0xFFE66100).withOpacity(0.3),
+    Color(0xFFE66100).withOpacity(0.3),
+    Color(0xFFE66100).withOpacity(0.3)
   ]);
 }
