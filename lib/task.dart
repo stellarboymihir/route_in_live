2441e3f2dc2e9592519d66e15b1eac1a4a34b3e2
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2023-12-16 21:32:02 +0530

    Holiday task day1

--- lib/task.dart
+++ lib/task.dart
@@ -11,6 +11,7 @@ class _TaskState extends State<Task> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 1st Part
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Expanded(
@@ -20,6 +21,7 @@ class _TaskState extends State<Task> {
                 padding: const EdgeInsets.all(8.0),
                 child: Expanded(
                   child: Row(
+                    // Upper Part  1st Row
                     children: [
                       Expanded(
                         child: Container(
@@ -59,6 +61,7 @@ class _TaskState extends State<Task> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
+                  // Upper Part 2nd Row
                   children: [
                     Expanded(
                       child: Container(
@@ -94,6 +97,8 @@ class _TaskState extends State<Task> {
                   ],
                 ),
               ),
+
+              //Lower Part
               Expanded(
                 child: Row(
                   children: [
@@ -102,38 +107,84 @@ class _TaskState extends State<Task> {
                         children: [
                           Expanded(
                             child: Container(
-                              padding: const EdgeInsets.all(20),
+                              // Lower Part 1st border radius
+                              padding: const EdgeInsets.all(40),
                               // height: 100,
                               // width: 100,
                               color: Colors.deepPurpleAccent.shade700,
                               child: Expanded(
                                 child: Container(
+                                  // Lower Part 2nd border radius
+                                  padding: const EdgeInsets.all(40),
                                   // height: 10,
                                   // width: 10,
                                   color: Colors.black,
-                                  child: Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        children: [
-                                          Container(
-                                            color: Colors.white,
-                                            // height: 10,
-                                            // width: 10,
-                                          ),
-                                          Container(
-                                            color: Colors.white,
-                                            // height: 10,
-                                            // width: 10,
-                                          ),
-                                          Container(
-                                            color: Colors.white,
-                                            // height: 10,
-                                            // width: 10,
-                                          ),
-                                        ],
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        child: Row(
+                                          children: [
+                                            Column(
+                                              children: [
+                                                Expanded(
+                                                  child: Container(
+                                                    color: Colors
+                                                        .yellowAccent.shade200,
+                                                    // height: 10,
+                                                    // width: 10,
+                                                    // child: Column(
+                                                    //   children: [
+                                                    //     Expanded(
+                                                    //       child: Container(
+                                                    //         padding:
+                                                    //             EdgeInsets.all(10),
+                                                    //         // height: 10,
+                                                    //         // width: 10,
+                                                    //         // color: Colors.blueAccent
+                                                    //         //     .shade100,
+                                                    //         // child: Container(
+                                                    //         //   // height: 10,
+                                                    //         //   // width: 10,
+                                                    //         //   color: Colors
+                                                    //         //       .purpleAccent
+                                                    //         //       .shade200,
+                                                    //         //
+                                                    //         //   child: Container(
+                                                    //         //     // height: 10,
+                                                    //         //     // width: 10,
+                                                    //         //     color: Colors
+                                                    //         //         .purpleAccent
+                                                    //         //         .shade700,
+                                                    //         //   ),
+                                                    //         // ),
+                                                    //       ),
+                                                    //     ),
+                                                    //   ],
+                                                    // ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Expanded(
+                                              child: Container(
+                                                color: Colors
+                                                    .yellowAccent.shade400,
+                                                // height: 10,
+                                                // width: 10,
+                                              ),
+                                            ),
+                                            Expanded(
+                                              child: Container(
+                                                color: Colors
+                                                    .yellowAccent.shade700,
+                                                // height: 10,
+                                                // width: 10,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                    ),
+                                    ],
                                   ),
                                 ),
                               ),
@@ -142,13 +193,6 @@ class _TaskState extends State<Task> {
                         ],
                       ),
                     ),
-                    // Expanded(
-                    //   child: Container(
-                    //     // height: 10,
-                    //     // width: 10,
-                    //     color: Colors.deepPurpleAccent.shade200,
-                    //   ),
-                    // ),
                   ],
                 ),
               ),
