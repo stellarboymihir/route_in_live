827fea5ddc2a94df1575547ae09a2e571cc50453
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2023-12-26 13:43:44 +0530

    Live Reservation dialog

--- lib/dashboard/home/regsiterbroadcast/LiveReservation.dart
+++ lib/dashboard/home/regsiterbroadcast/LiveReservation.dart
@@ -15,7 +15,15 @@ class _LiveReservationState extends State<LiveReservation> {
   TextEditingController broadcastNameController = TextEditingController();
   TextEditingController broadcastDateController = TextEditingController();
   TextEditingController broadcastTimeController = TextEditingController();
-  TextEditingController AttachController = TextEditingController();
+  TextEditingController attachController = TextEditingController();
+
+  final List<String> _list = [
+    '18:00 You can make a',
+    '18:00 You can’t make',
+    '18:00 You can makes a'
+  ];
+
+  String? _downVal;
 
   int _char = 0;
 
@@ -23,14 +31,32 @@ class _LiveReservationState extends State<LiveReservation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Application for live broadcast reservation',
-          style: MyStyle.tx14W.copyWith(
-            color: MyColor.black,
-            fontSize: 13,
+        title: Row(
+          children: [
+            Image.asset('assets/icons/back.png'),
+            const SizedBox(
+              width: 20,
+            ),
+            Text(
+              'Application for live broadcast reservation',
+              style: MyStyle.tx14W.copyWith(
+                color: MyColor.black,
+                fontFamily: 'NotoSansKR-Regular',
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
+
+        automaticallyImplyLeading: false,
+
+        // titleSpacing: 6,
+        shape: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: MyColor.black.withOpacity(0.1),
           ),
         ),
-        elevation: 1,
+        // elevation: 0,
         centerTitle: true,
       ),
       body: Form(
@@ -52,7 +78,7 @@ class _LiveReservationState extends State<LiveReservation> {
                   'The broadcast name is the content that is shown'
                   ' to users at the time of notice.',
                   style: MyStyle.tx10.copyWith(
-                    fontSize: 8,
+                    fontSize: 10,
                     fontFamily: 'NotoSansKR-Regular',
                     decoration: TextDecoration.none,
                   ),
@@ -118,7 +144,9 @@ class _LiveReservationState extends State<LiveReservation> {
                 ),
                 Row(
                   children: [
-                    Expanded(
+                    SizedBox(
+                      height: 40,
+                      width: 140,
                       child: TextFormField(
                         validator: (value) {
                           if (value == null || value.isEmpty) {
@@ -157,16 +185,12 @@ class _LiveReservationState extends State<LiveReservation> {
                       ),
                     ),
                     const SizedBox(
-                      width: 20,
+                      width: 5,
                     ),
-                    Expanded(
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter some text';
-                          }
-                          return null;
-                        },
+                    SizedBox(
+                      height: 40,
+                      width: 198,
+                      child: DropdownButtonFormField<String>(
                         style: MyStyle.tx14O,
                         decoration: InputDecoration(
                           enabledBorder: OutlineInputBorder(
@@ -183,22 +207,37 @@ class _LiveReservationState extends State<LiveReservation> {
                           ),
                           contentPadding: const EdgeInsets.all(8.0),
                           border: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                            color: MyColor.orange,
-                          )),
-                          hintText: '00:00',
-                          suffixIcon: Icon(
-                            Icons.keyboard_arrow_down_outlined,
-                            color: MyColor.orangeO,
+                            borderSide: BorderSide(
+                              color: MyColor.orange,
+                            ),
                           ),
+                          hintText: '00:00',
                           hintStyle:
                               MyStyle.tx14O.copyWith(color: MyColor.orangeO),
                         ),
-                        controller: broadcastTimeController,
-                        keyboardType: TextInputType.datetime,
-                        onChanged: (value) {
-                          setState(() {});
+                        value: _downVal,
+                        elevation: 0,
+                        // style: const TextStyle(color: MyColor.orange),
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: MyColor.orange,
+                        ),
+                        onChanged: (String? value) {
+                          print(value);
+                          setState(() {
+                            _downVal = null;
+                            _downVal = value!;
+                          });
                         },
+                        items: _list.map<DropdownMenuItem<String>>((list) {
+                          print(list);
+                          return DropdownMenuItem<String>(
+                              value: list,
+                              child: Text(
+                                list,
+                                style: const TextStyle(color: MyColor.orange),
+                              ));
+                        }).toList(),
                       ),
                     ),
                   ],
@@ -297,7 +336,7 @@ class _LiveReservationState extends State<LiveReservation> {
                   ),
                 ),
                 const SizedBox(
-                  height: 8,
+                  height: 12,
                 ),
                 TextFormField(
                   validator: (value) {
@@ -326,10 +365,26 @@ class _LiveReservationState extends State<LiveReservation> {
                       color: MyColor.orange,
                     )),
                     hintText: 'Attach videos and images',
-                    suffixIcon: Image.asset('assets/icons/Attach.png'),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return liveBroadcastDialog();
+                            });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Image.asset(
+                          'assets/icons/Attach.png',
+                          height: 15,
+                          width: 15,
+                        ),
+                      ),
+                    ),
                     hintStyle: MyStyle.tx14O.copyWith(color: MyColor.orangeO),
                   ),
-                  controller: AttachController,
+                  controller: attachController,
                   keyboardType: TextInputType.text,
                   onChanged: (value) {
                     setState(() {});
@@ -356,12 +411,15 @@ class _LiveReservationState extends State<LiveReservation> {
                   style: MyStyle.tx16O
                       .copyWith(fontFamily: 'NotoSansKR-Regular', fontSize: 15),
                 ),
-                const Text(
+                const SizedBox(
+                  height: 2,
+                ),
+                Text(
                   'Search for the product you want to sell and register it.',
-                  style: MyStyle.tx12B,
+                  style: MyStyle.tx12B.copyWith(fontSize: 11),
                 ),
                 const SizedBox(
-                  height: 8,
+                  height: 12,
                 ),
 
                 // *** Buttons ***
@@ -370,15 +428,18 @@ class _LiveReservationState extends State<LiveReservation> {
                   decoration: const BoxDecoration(
                     color: MyColor.dorange,
                   ),
-                  child: const Center(
+                  child: Center(
                     child: Text(
                       'Select products to sell',
-                      style: MyStyle.tx14W,
+                      style: MyStyle.tx14W.copyWith(
+                        fontFamily: 'NotoSansKR-Regular',
+                      ),
                     ),
                   ),
                 ),
-                const Divider(
-                  thickness: 0,
+                Divider(
+                  thickness: 0.5,
+                  color: MyColor.black.withOpacity(0.1),
                 ),
                 const SizedBox(
                   height: 6,
@@ -416,6 +477,139 @@ class _LiveReservationState extends State<LiveReservation> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget liveBroadcastDialog() {
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // *** Uploading an Image ***
+          Row(
+            children: [
+              Image.asset(
+                'assets/icons/Shape.png',
+                height: 18,
+                width: 18,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                'Uploading an image',
+                style: MyStyle.tx16B.copyWith(
+                    color: MyColor.black, fontFamily: 'NotoSansKR-Medium'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            ' The image is an image used as a thumbnail,'
+            ' and only files up to 10MB or less can be uploaded.',
+            style: MyStyle.tx12B.copyWith(
+              color: MyColor.black.withOpacity(0.7),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 40,
+            width: 330,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1),
+              border: Border.all(
+                color: MyColor.orange,
+                width: 1,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                'Uploading an image',
+                style: MyStyle.tx14O,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+
+          // *** Uploading an Video ***
+          Row(
+            children: [
+              Image.asset(
+                'assets/icons/Video.png',
+                height: 18,
+                width: 18,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                'Upload a preview video',
+                style: MyStyle.tx16B.copyWith(
+                    color: MyColor.black, fontFamily: 'NotoSansKR-Medium'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            'The preview video is a video that viewers can watch '
+            'before watching the broadcast, and only files up to '
+            '350MB or less can be uploaded.',
+            style: MyStyle.tx12B.copyWith(
+              fontSize: 11,
+              color: MyColor.black.withOpacity(0.7),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 40,
+            width: 330,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1),
+              border: Border.all(
+                color: MyColor.orange,
+                width: 1,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                'Upload the preview video',
+                style: MyStyle.tx14O,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+
+          //   *** Text ***
+          Center(
+            child: Text(
+              'Images and preview videos are optional. \n'
+              'Up to one image and one preview can be uploaded.',
+              textAlign: TextAlign.center,
+              style: MyStyle.tx12O.copyWith(
+                fontWeight: FontWeight.w400,
+                fontSize: 11,
+              ),
+            ),
+          ),
+          RichText(
+            text: const TextSpan(
+                text: 'Images and preview videos are optional. \n'
+                    'Up to one image and one preview can be uploaded.'),
+          ),
+        ],
       ),
     );
   }
